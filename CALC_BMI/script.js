526458f46f8e1240d18091921f04bf60c27451f7
JulioCesarLB <julio.lifeintech@gmail.com>
2022-04-13 16:56:02 -0300

    Finalizando o JS

--- CALC_BMI/script.js
+++ CALC_BMI/script.js
@@ -45,5 +45,46 @@ function calc_BMI(){
     
     height = height/100;
     var BMI =weight/(height*height);
+
+    BMI = BMI.toFixed(2);
+    height = height.toFixed(2);
+
+    toggle();
+    alter(height,weight,age,BMI,gen);
    
+}
+
+function alter(height,weight,age,BMI, gen){
+    document.querySelector(".bmi").textContent= BMI;
+    document.querySelector(".result_age").textContent= age;
+    document.querySelector(".result_gen").textContent= gen;
+    document.querySelector(".result_wei").textContent= weight;
+    document.querySelector(".result_hei").textContent= height;
+
+    if(BMI>=40){
+        document.querySelector(".result_bmi").textContent="Obesity class III";
+        document.querySelector(".result_bmi").style.color = "#950000";
+    }else if(BMI>=35){
+        document.querySelector(".result_bmi").textContent="Obesity class II";
+        document.querySelector(".result_bmi").style.color = "#B30000";
+    }else if(BMI>=30){
+        document.querySelector(".result_bmi").textContent="Obesity class I";
+        document.querySelector(".result_bmi").style.color = "#FF0000";
+    }else if(BMI>=25){
+        document.querySelector(".result_bmi").textContent="Pre-obesity";
+        document.querySelector(".result_bmi").style.color = "#FF4D00";
+    }else if(BMI>=18.5){
+        document.querySelector(".result_bmi").textContent="Normal weight";
+        document.querySelector(".result_bmi").style.color = "#00FF00";
+    }else{
+        document.querySelector(".result_bmi").textContent="Underweight";
+        document.querySelector(".result_bmi").style.color = "#00025E";
+    }
+
+}
+function toggle(){
+    var element = document.getElementById("result");
+  element.classList.toggle("invisible");
+    var element2 = document.getElementById("calculation");
+  element2.classList.toggle("invisible");
 }
